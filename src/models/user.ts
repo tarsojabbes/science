f4f282acd9df1ebc0a5ea22573c430247d49f4e3
Tarso Jabbes <tarsojabbes@gmail.com>
2025-07-07 09:47:42 -0300

    Add code

---
+++ src/models/user.ts
@@ -0,0 +1,47 @@
+import { Model, DataTypes, Optional } from 'sequelize';
+import sequelize from '../config/database';
+
+interface UserAttributes {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+
+export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+}
+
+
+// Inicialize o modelo com os campos no banco
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users", 
+    timestamps: false,
+  }
+);
